bed5863e02fe1b4cb0cc0d99bd429326702556ee
Vilmos Csibi <csibi.vili@gmail.com>
2018-05-26 20:34:52 +0200

    add(IIssue): create interface for issue

---
+++ EmarsysDueDateCalculator/Models/Issue/IIssue.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace EmarsysDueDateCalculator.Models.Issue
+{
+    public interface IIssue
+    {
+        DateTime GetSubmitDateInUtc();
+        int GetDedicatedTimeInHours();
+        void SetSubmitDateInUtc(DateTime timestamp);
+        void SetSubmitDateInLocalTime(DateTime timestamp);
+        void SetDedicatedTimeInHours(int hours);
+        void SetDedicatedTimeInDays(int days);
+        void SetDeicatedTimeInWeeks(int weeks);
+    }
+}
