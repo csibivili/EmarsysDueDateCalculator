1fdcaffbdcbd24c7f5441625f454ccbd1fae1b71
Vilmos Csibi <csibi.vili@gmail.com>
2018-05-26 21:22:54 +0200

    add(BugFixTests): add test project + first success test

---
+++ EmarsysDueDateCalculator.Tests/BugFixTest.cs
@@ -0,0 +1,24 @@
+using System;
+using EmarsysDueDateCalculator.Models.Issue;
+using Xunit;
+
+namespace EmarsysDueDateCalculator.Tests
+{
+    public class BugFixTest
+    {
+        [Fact]
+        public void SetSubmitDateInUtc_DuringWorkingHours_OK()
+        {
+            var bugFix = new BugFix();
+
+            var timestamp = new DateTime(2018,5,28,10,0,0);
+
+            bugFix.SetSubmitDateInUtc(timestamp);
+
+            var expected = timestamp;
+            var actual = bugFix.GetSubmitDateInUtc();
+
+            Assert.Equal(expected, actual);
+        }
+    }
+}
