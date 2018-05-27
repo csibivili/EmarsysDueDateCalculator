f923bc872bf421fac23c4b2be227a81b91f3862e
Vilmos Csibi <csibi.vili@gmail.com>
2018-05-27 13:54:11 +0200

    add(IIssueDueDateCalculator): create interface for duedate calculator

---
+++ EmarsysDueDateCalculator/Services/DueDateCalculator/IIssueDueDateCalculator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using EmarsysDueDateCalculator.Models.Issue;
+
+namespace EmarsysDueDateCalculator.Services.DueDateCalculator
+{
+    public interface IIssueDueDateCalculator
+    {
+        DateTime CalculateDueDate(IIssue issue);
+    }
+}
